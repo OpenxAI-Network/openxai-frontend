acf57369f3e3824220f3afc9fe49c85a7d62850f
Plopmenz <plopmenz@gmail.com>
2025-02-12 18:26:16 +0100

    fullscreen iframe

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,11 @@
+import { FullscreenIframe } from "@/components/fullscreen-iframe"
 import { Header } from "@/components/Header"
 
 export default function IndexPage() {
   return (
     <>
       <Header />
-      <div className="container flex min-h-screen flex-col items-center">
-        <div className="h-screen w-full overflow-hidden">
-          <iframe
-            src="https://exuberant-deck-437452.framer.app/"
-            title="OpenxAI Vision"
-            loading="lazy"
-            style={{ border: 0, marginTop: '-70px' }}
-            className="h-[calc(100%+50px)] w-full"
-            sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
-          />
-        </div>
-      </div>
+      <FullscreenIframe src="https://exuberant-deck-437452.framer.app/" />
     </>
   )
 }
