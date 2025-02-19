ad91c240c2bf48da804b2ee33202b42b9d5655de
johnforfar <46335740+johnforfar@users.noreply.github.com>
2025-02-19 14:07:52 +0800

    reduce padding

--- components/layouts/MobileResponsiveWrapper.tsx
+++ components/layouts/MobileResponsiveWrapper.tsx
@@ -4,8 +4,13 @@ export function MobileResponsiveWrapper({ children }: { children: React.ReactNod
   return (
     <div className="flex min-h-screen bg-[radial-gradient(ellipse_at_center_top,_rgba(27,37,56,0.9)_0%,_#151516_100%)]">
       <SideMenu />
-      <main className="min-w-[320px] flex-1 overflow-x-auto p-4 pt-28 [@media(max-width:500px)]:pt-24 [@media(max-width:960px)]:pt-48 [@media(min-width:960px)]:ml-[234px] [@media(min-width:960px)]:p-8 [@media(min-width:960px)]:pt-32">
-        <div className="px-safe">
+      <main className="min-w-[320px] flex-1 overflow-x-auto px-2 pt-28 
+        [@media(max-width:500px)]:pt-24 
+        [@media(max-width:960px)]:pt-48 
+        [@media(min-width:960px)]:ml-[234px] 
+        [@media(min-width:960px)]:px-4 
+        [@media(min-width:960px)]:pt-32">
+        <div>
           {children}
         </div>
       </main>
